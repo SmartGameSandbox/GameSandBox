bef46be883181af58d730c51cb9fc93232aa8fdf
Tomo Kaneko <tkaneko@my.bcit.ca>
2022-10-12 14:32:31 -0700

    refactor App.jsx to be functional components

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RoomCreation from './components/rooms/roomCreation';
 import Room from './components/rooms/room';
 
-export default class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<RoomCreation />} />
-          <Route path='createroom' element={<RoomCreation />} />
-          <Route path='room' element={<Room />} />
-        </Routes>
-      </BrowserRouter>
-    );
-  }
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<RoomCreation />} />
+        <Route path='createroom' element={<RoomCreation />} />
+        <Route path='room' element={<Room />} />
+      </Routes>
+    </BrowserRouter>
+  ) 
 }
+
+export default App
