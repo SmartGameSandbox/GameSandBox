e8064e466b767d156619d01443943284e7ead3a9
Heather Lee <leeh1102@gmail.com>
2022-10-12 23:07:49 -0700

    Cretae login page and reset main route to login

---
+++ src/components/loginComponent/loginComponent.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import axios from "axios";
+import styles from "./loginStyle";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+const LoginComponent = () => {
+  const [usernameInputText, setUsernameInputText] = React.useState("");
+  const handleUsernameTextInputChange = (event) => {
+    setUsernameInputText(event.target.value);
+  };
+  const [passwordInputText, setPasswordInputText] = React.useState("");
+  const handlePasswordTextInputChange = (event) => {
+    setPasswordInputText(event.target.value);
+  };
+
+  return (
+    <>
+      <Box
+        sx={styles.roomBoxStyle}
+        id="create-room-container"
+        component="form"
+        autoComplete="off"
+      >
+        <h1>Login to your account</h1>
+        <h4>Hey, good to see you again!</h4>
+        <div>
+          <TextField
+            id="username-input"
+            sx={styles.textFieldStyle}
+            placeholder="Please enter your username"
+            value={usernameInputText}
+            onChange={handleUsernameTextInputChange}
+            className="text-field"
+            required
+            label="Username"
+            size="large"
+          />
+          <br />
+          <TextField
+            id="password-input"
+            sx={styles.textFieldStyle}
+            placeholder="Please enter your password"
+            value={passwordInputText}
+            onChange={handlePasswordTextInputChange}
+            className="text-field"
+            required
+            label="Password"
+            type={"password"}
+            size="large"
+          />
+        </div>
+        <div sx={styles.forgotPasswordStyle}>
+          <div>
+            <Button sx={styles.forgotPasswordStyle}>Forgot password?</Button>
+          </div>
+          <div>
+            <Button
+              variant="contained"
+              sx={styles.signInButtonStyle}
+              // onClick={() => {
+              //   createRoom();
+              // }}
+            >
+              Sign in
+            </Button>
+          </div>
+        </div>
+
+        <div>
+          <span>Not registered yet?</span>
+          <Button>Cretae an Account</Button>
+        </div>
+      </Box>
+    </>
+  );
+};
+
+export default LoginComponent;
---
+++ src/components/loginComponent/loginStyle.js
@@ -0,0 +1,32 @@
+const styles = {
+    textFieldStyle: {
+        mb: 2.0,
+        mt: 1.5,
+        width: 300
+    },
+    roomBoxStyle: {
+        height: 400,
+        width: 400,
+        ml: "calc(50% - 200px)",
+        border: "2px solid lightseagreen",
+        borderRadius: 10,
+        mt: 15,
+        pt: 3,
+        pl: 5
+    },
+    forgotPasswordStyle: {
+        display: "flex",
+        justifyContent: "flex-between",
+        // ml: 6.0,
+        // padding: 0,
+        // margin: 0,
+
+
+    },
+    signInButtonStyle: {
+        bgcolor: "lightseagreen",
+
+    }
+}
+
+export default styles;
