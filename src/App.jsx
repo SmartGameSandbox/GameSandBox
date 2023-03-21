5e23aee6008a4c1b42efb8df1865a384750a3dd3
MitchellWatson <90720653+MitchellWatson@users.noreply.github.com>
2023-03-21 14:07:54 -0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import RoomCreation from "./components/rooms/createRoom";
 import Room from "./components/rooms/room";
+// import JoinRoom from "./components/rooms/joinRoom";
 import MyGames from "./components/myGames/myGames";
 import Login from "./components/login/login";
 import Logout from "./components/logout/logout";
@@ -10,6 +11,7 @@ import Register from "./components/register/register";
 import BuildGamePage from "./components/buildGame/buildGamePage";
 import { ReactSession } from "react-client-session";
 import Dashboard from './components/dashboard/dashboard';
+import Games from './components/games/games.jsx';
 ReactSession.setStoreType("localStorage");
 
 const App = () => {
@@ -34,10 +36,18 @@ const App = () => {
             path="/dashboard"
             element={isAuthed() ? <Dashboard /> : <Navigate to="/login" />}
           />
+          <Route 
+            path="/games"
+            element={isAuthed() ? <Games /> : <Navigate to="/login" />}
+          />
           <Route
             path="/createroom"
             element={isAuthed() ? <RoomCreation /> : <Navigate to="/login" />}
           />
+          {/* <Route
+            path="/joinroom"
+            element={isAuthed() ? <JoinRoom /> : <Navigate to="/login" />}
+          /> */}
           <Route
             path="/mygames"
             element={isAuthed() ? <MyGames /> : <Navigate to="/login" />}
