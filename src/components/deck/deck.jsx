60dfb6fffe586fc5870717571754da6f5be17dd0
Eric H <erichemming11@gmail.com>
2023-05-17 19:53:48 -0700

    Refactored onDragEnd function into gameaction.jsx deck, token, piece.jsx now use common gameaction.jsx functions

--- src/components/deck/deck.jsx
+++ src/components/deck/deck.jsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import Card from "../card/card";
 import * as Constants from "../../util/constants";
 import { Rect } from "react-konva";
-import { onDragMoveCardGA } from "../gameaction/gameaction";
+import { onDragMoveCardGA, onDragEndGA } from "../gameaction/gameaction";
 
 
 // deck data
@@ -15,59 +15,7 @@ const Deck = ({ tableData, deckIndex, setCanEmit, setTableData, emitMouseChange 
   };
 
   const onDragEnd = (e, cardID) => {
-    const position = e.target.attrs;
-    setCanEmit(true);
-    if (
-      position.x >= Constants.DECK_STARTING_POSITION_X - Constants.CARD_WIDTH &&
-      position.x <=
-        Constants.DECK_STARTING_POSITION_X + Constants.DECK_AREA_WIDTH &&
-      position.y >=
-        Constants.DECK_STARTING_POSITION_Y - Constants.CARD_HEIGHT &&
-      position.y <=
-        Constants.DECK_STARTING_POSITION_Y + Constants.DECK_AREA_HEIGHT
-    ) {
-      // deck area movement
-      setTableData((prevTable) => {
-        prevTable.deck[deckIndex] = prevTable.deck[deckIndex].map((card) => {
-          if (card.id === cardID) {
-            card.x =
-              Constants.DECK_STARTING_POSITION_X + Constants.DECK_PADDING;
-            card.y =
-              Constants.DECK_STARTING_POSITION_Y + Constants.DECK_PADDING;
-          }
-          return card;
-        });
-        return { ...prevTable };
-      });
-    } else if (
-      position.y >
-      Constants.CANVAS_HEIGHT -
-        Constants.HAND_HEIGHT -
-        0.5 * Constants.CARD_HEIGHT
-    ) {
-      setTableData((prevTable) => {
-        // find card in tableData.deck
-        const found = prevTable.deck[deckIndex].find((card) => card.id === cardID);
-        // add card to hand
-        prevTable.hand.push(found);
-        found.x = e.target.attrs.x
-        found.y = e.target.attrs.y
-        prevTable.deck[deckIndex] = prevTable.deck[deckIndex].filter((card) => card.id !== cardID);
-        return { ...prevTable };
-      });
-    } else {
-      // deck to table
-      setTableData((prevTable) => {
-        // find card in tableData.deck
-        const found = prevTable.deck[deckIndex].find((card) => card.id === cardID);
-        found.x = position.x;
-        found.y = position.y;
-        // add card to hand
-        prevTable.cards.push(found);
-        prevTable.deck[deckIndex] = prevTable.deck[deckIndex].filter((card) => card.id !== cardID);
-        return { ...prevTable };
-      });
-    }
+    onDragEndGA(e, cardID, deckIndex, setCanEmit, setTableData, emitMouseChange, "deck");
   };
 
   return (
--- src/components/deck/token.jsx
+++ src/components/deck/token.jsx
@@ -1,7 +1,7 @@
 import Card from "../card/card";
 import * as Constants from "../../util/constants";
 import { Rect, Text } from "react-konva";
-import { onDragMoveCardGA } from "../gameaction/gameaction";
+import { onDragMoveCardGA, onDragEndGA } from "../gameaction/gameaction";
 
 
 // deck data
@@ -12,63 +12,7 @@ const Token = ({ tableData, deckIndex, setCanEmit, setTableData, emitMouseChange
   };
 
   const onDragEnd = (e, cardID) => {
-    const position = e.target.attrs;
-    setCanEmit(true);
-    if (
-      position.x >= Constants.DECK_STARTING_POSITION_X - Constants.CARD_WIDTH &&
-      position.x <=
-        Constants.DECK_STARTING_POSITION_X + Constants.DECK_AREA_WIDTH &&
-      position.y >=
-        Constants.DECK_STARTING_POSITION_Y - Constants.CARD_HEIGHT &&
-      position.y <=
-        Constants.DECK_STARTING_POSITION_Y + Constants.DECK_AREA_HEIGHT
-    ) {
-      // deck area movement
-      setTableData((prevTable) => {
-        prevTable.tokens[deckIndex] = prevTable.tokens[deckIndex].map((card) => {
-          if (card.id === cardID) {
-            card.x =
-              Constants.DECK_STARTING_POSITION_X + Constants.DECK_PADDING;
-            card.y =
-              Constants.DECK_STARTING_POSITION_Y + Constants.DECK_PADDING;
-          }
-          return card;
-        });
-        return { ...prevTable };
-      });
-    } else if (
-      position.y >
-      Constants.CANVAS_HEIGHT -
-        Constants.HAND_HEIGHT -
-        0.5 * Constants.CARD_HEIGHT
-    ) {
-      setTableData((prevTable) => {
-        // find card in tableData.deck
-        const found = prevTable.tokens[deckIndex].find((card) => card.id === cardID);
-        // add card to hand
-        prevTable.hand.push(found);
-        found.x =
-          Constants.HAND_PADDING_X +
-          (prevTable.hand.length - 1) * Constants.HAND_CARD_GAP;
-        found.y =
-          Constants.CANVAS_HEIGHT -
-          Constants.HAND_HEIGHT +
-          Constants.HAND_PADDING_Y;
-        prevTable.tokens[deckIndex] = prevTable.tokens[deckIndex].filter((card) => card.id !== cardID);
-        return { ...prevTable };
-      });
-    } else {
-      // deck to table
-      setTableData((prevTable) => {
-        // find card in tableData.deck
-        const found = prevTable.tokens[deckIndex].find((card) => card.id === cardID);
-        found.x = position.x;
-        found.y = position.y;
-        prevTable.cards.push(found);
-        prevTable.tokens[deckIndex] = prevTable.tokens[deckIndex].filter((card) => card.id !== cardID);
-        return { ...prevTable };
-      });
-    }
+    onDragEndGA(e, cardID, deckIndex, setCanEmit, setTableData, emitMouseChange, "tokens");
   };
 
   return (
